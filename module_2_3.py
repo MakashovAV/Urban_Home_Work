80a189a1ef15fa5f28c9c70b4558f9f3d0c2ff9e
MakashovAV <mak2440@yandex.ru>
2024-07-19 07:37:47 +0700

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -4,10 +4,9 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 x = 0
 
-while x <= len(my_list):
+while x < len(my_list):
     if my_list[x] > 0:
         print(my_list[x])
     if my_list[x] < 0:
         break
     x = x + 1
-
