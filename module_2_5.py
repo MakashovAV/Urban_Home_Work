3e098d37eaf854808b822f6575aa249bbf894e52
MakashovAV <mak2440@yandex.ru>
2024-07-19 22:00:43 +0700

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,1 +1,15 @@
+#Цель: закрепить навык написания функций и их вызовов.
+#Задача "Матрица воплоти":
 
+def get_matrix (n,m,value):
+    matrix = []
+    for i in range(n):
+        print('I=', i)
+        matrix.append([])
+        print(matrix)
+        for j in range(m):
+            print('j= ', j)
+ #           matrix[0][i].append = value
+            print(matrix)
+            
+get_matrix(2, 3, 5)
