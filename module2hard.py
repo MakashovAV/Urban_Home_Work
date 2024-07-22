c7804ff60795e9afa3736bcb2781c18e1c644655
MakashovAV <mak2440@yandex.ru>
2024-07-22 19:13:36 +0700

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,27 +1,24 @@
-n = int(input('введите число от 3 до 20'))
+n = int(input('введите число от 3 до 20: '))
 result = ''
 result_ = []
-divider =[]
-for i in range(3, n+1):
+divider = []
+for i in range(3, n + 1):                               # Находим возможные делители
     if n % i == 0:
         divider.append(i)
-print(divider)
-if n % 2 == 0:
+if n % 2 == 0:                                          # ограничиваем перебор первого числа
     x = n // 2
 else:
-    x = n // 2 +1
+    x = n // 2 + 1
 
-for i in range(1, x):
-    for j in range(1, n):
+for i in range(1, x):                                   # Перебираем первое число
+    for j in range(1, n):                               # Перебираем второе число
         for k in divider:
-            if i + j == k and i != j:
-                result_.append([i,j])
-                for x in result_:
+            if i + j == k and i != j:                   # делаем список пар
+                result_.append([i, j])
+                for x in result_:                       # Сразу убираем "обратные" пары
                     if x == [j, i]:
                         result_.remove([i, j])
-                        
+
 for i in result_:
-    result += (str(i[0]) + str(i[1]))
+    result += (str(i[0]) + str(i[1]))                   # Склеиваим пары в пароль
 print(result)
-# 13 14 19 119 23 28 218 37 317 46 416 515 614 713 812 911
-                
